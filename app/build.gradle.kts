a9396dd328c1ea6e415068e9c42b068308f60cad
Alpay Ozer <alpay.ozer@student.hogent.be>
2024-10-31 17:50:02 +0100

    KSP is now compatible with kotlin
    updated kotlin version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,7 +2,7 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
-    id("org.jetbrains.kotlin.plugin.serialization") version "1.8.10"
+    id("org.jetbrains.kotlin.plugin.serialization") version "2.0.21"
     id("com.google.devtools.ksp") version "2.0.21-1.0.25"
 }
 
