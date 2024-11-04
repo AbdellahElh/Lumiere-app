1ebd1ca56a2db717f644065204093d386bdd4e31
Alpay Ozer <alpay.ozer@student.hogent.be>
2024-11-04 18:42:27 +0100

    Pipeline

--- app/src/main/java/com/example/riseandroid/repository/ApiResource.kt
+++ app/src/main/java/com/example/riseandroid/repository/ApiResource.kt
@@ -8,4 +8,5 @@ sealed class APIResource<T>(val data: T? = null, val message: String? = null) {
     class Success<T>(data: T?) : APIResource<T>(data)
     class Error<T>(message: String, data: T? = null) : APIResource<T>(data, message)
 
+
 }
