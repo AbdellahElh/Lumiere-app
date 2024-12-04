d0481b52fe06b27730d78a6f19f5e6a53aeecb9e
Miel-DR <miel.deroeck@student.hogent.be>
2024-12-04 20:37:21 +0100

    Testcase error fix
    Thanks to the review of @georgiana , I have now updated the testcases to implement retrofit2 mock to mock the api response.
    
    Now the TenturncardRepoistoryTest runs as expected.

--- app/src/test/java/com/example/riseandroid/fake/FakeTenturncardApi.kt
+++ app/src/test/java/com/example/riseandroid/fake/FakeTenturncardApi.kt
@@ -1,19 +1,24 @@
 package com.example.riseandroid.fake
 
+import coil.request.SuccessResult
 import com.example.riseandroid.model.Tenturncard
 import com.example.riseandroid.network.TenturncardApi
+import okhttp3.ResponseBody.Companion.toResponseBody
+import retrofit2.Call
+import retrofit2.Response
+import retrofit2.mock.Calls
 
 class FakeTenturncardApi : TenturncardApi {
     override suspend fun getTenturncards(): List<Tenturncard> {
         TODO("Not yet implemented")
     }
 
-    override suspend fun addTenturncard(activationCode: String): Result<Unit> {
-        if(activationCode == "testCode") {
-            return Result.success(Unit)
+    override fun addTenturncard(activationCode: String): Call<Unit> {
+        return if(activationCode == "testCode") {
+            Calls.response(Response.success(Unit))
         }
         else{
-            return Result.failure(IllegalArgumentException())
+            Calls.response(Response.error(400, "Invalid activation code".toResponseBody(null)))
         }
     }
 }
